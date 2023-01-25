6a7b98cc97b5163a468e0aa4f233f788dc90a87c
Dwiki Ramadhan <dwikibyy@gmail.com>
2023-01-25 10:49:21 +0700

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app color="#111111" dark flat class="px-8">
+    <v-btn icon>
+      <v-icon color="white">fas fa-cat</v-icon>
+    </v-btn>
+    <v-spacer></v-spacer>
+    <v-btn text @click="scroll('home')">Home</v-btn>
+    <v-btn text @click="scroll('project')">Project</v-btn>
+    <v-btn text @click="scroll('about')">About</v-btn>
+    <v-btn text @click="scroll('contact')">Contact</v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  methods: {
+    scroll(refName) {
+      const element = document.getElementById(refName);
+      element.scrollIntoView({ behavior: "smooth" });
+    },
+  },
+};
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from "vue";
+import Vuetify from "vuetify/lib/framework";
+
+import "@fortawesome/fontawesome-free/css/all.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+  icons: {
+    iconfont: "fa" || "md" || "mdi",
+  },
+  theme: {
+    themes: {
+      dark: {
+        background: "#111111",
+      },
+    },
+  },
+});
